516cc216ad096736a1842e7f60f8cf58303ee3ef
Nikolay Loboda <nick12137@gmail.com>
2023-02-08 20:01:07 -0600

    fixed all issues

---
+++ SelectionStatementExercise/ProgramFixed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SelectionStatementExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the number guessing game!");
+            Console.WriteLine("See if you can guess the correct number!");
+            Console.WriteLine("What is the upper limit for the range of the number");
+
+            var userRespone = Console.ReadLine();
+
+            var upperLimit = int.Parse(userRespone);
+
+
+            var random = new Random();
+            var number = random.Next(1, upperLimit);
+
+            Console.WriteLine("Input your guess: ");
+
+            var guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Your guess is too high! Sorry");
+
+            }
+
+            else if (guess < number)
+
+            {
+                Console.WriteLine("Your guess is too low! Sorry ");
+
+            }
+            else
+            {
+                Console.WriteLine("You are Correct!");
+
+
+            }
+        }
+    }
+}
